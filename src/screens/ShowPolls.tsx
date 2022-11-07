06025db871368bba8a7a317859cb43a1a029ed77
Matheus Pazinati <matheuspazinati@gmail.com>
2022-11-07 01:52:58 -0300

    Cria funcionalidade para visualizar os boloes do usuario autenticado

--- src/screens/ShowPolls.tsx
+++ src/screens/ShowPolls.tsx
@@ -1,14 +1,50 @@
-import { VStack, Icon } from "native-base";
+import { useState, useCallback } from "react";
 
-import { useNavigation } from '@react-navigation/native'
+import { VStack, Icon, useToast, FlatList } from "native-base";
+
+import { useNavigation, useFocusEffect } from '@react-navigation/native'
 
 import { Octicons } from '@expo/vector-icons'
 
+import { api } from "../services/api";
+
 import { Button } from "../components/Button";
 import { Header } from "../components/Header";
+import { PoolCard, PoolCardProps } from '../components/PoolCard'
+import { EmptyPoolList } from "../components/EmptyPoolList";
+import { Loading } from '../components/Loading'
+
 
 export function ShowPolls() {
   const navigation = useNavigation();
+
+  const [polls, setPolls] = useState<PoolCardProps[]>([])
+  const [isLoading, setIsLoading] = useState(true)
+
+  const toast = useToast()
+
+  async function getPolls() {
+    try {
+      setIsLoading(true)
+      const response = await api.get('/pools')
+      setPolls(response.data.pools)
+
+    } catch (error) {
+      console.log(error)
+      toast.show({
+        title: 'Não foi possível carregar os bolões',
+        placement: 'top',
+        bgColor: 'red.500'
+      })
+
+    } finally {
+      setIsLoading(false)
+    }
+  }
+
+  useFocusEffect(useCallback(() => {
+    getPolls()
+  }, []))
 
   return (
     <VStack flex={1} bgColor="gray.900">
@@ -20,6 +56,20 @@ export function ShowPolls() {
           onPress={() => navigation.navigate('find')}
         />
       </VStack>
+
+      {
+        isLoading ? <Loading /> :
+        <FlatList
+          data={polls}
+          keyExtractor={item => item.id}
+          renderItem={({ item }) => <PoolCard data={item} />}
+          px={5}
+          showsVerticalScrollIndicator={false}
+          _contentContainerStyle={{ pb: 10 }}
+          ListEmptyComponent={() => <EmptyPoolList />}
+        />
+      }
+
     </VStack>
   )
 }
